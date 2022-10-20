a76040acb210a001bf846fe2cf396917c4d868cb
Hossein Mosalli <mosali@payrad.org>
2022-10-20 11:36:24 +0330

    fix: empty login style file

--- src/pages/Login/LoginStyles.jsx
+++ src/pages/Login/LoginStyles.jsx
@@ -6,17 +6,7 @@ const options = {
 
 export default makeStyles(
 	() => ({
-		container: {
-			width: "100%",
-			height: "100vh",
-			overflow: "hidden",
-			display: "flex",
-			alignItems: "stretch",
-		},
-		main: {
-			width: "100%",
-			overflow: "hidden",
-		},
+		root: {},
 	}),
 	options
 );
