83450911664f5c269d8ebdceb2a0c30061a2cf57
Hossein Mosalli <mosali@payrad.org>
2022-10-20 11:22:27 +0330

    feat: add mui theme

---
+++ src/common/mui-theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    direction: "rtl",
+	typography: {
+		button: {
+			textTransform: "none",
+		},
+	},
+});
+
+export default theme;
