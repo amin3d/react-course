74a43abc8c19c58af287ce18be0e1dfcc30dbe2d
Hossein Mosalli <mosali@payrad.org>
2022-12-01 09:12:45 +0330

    feat: adds delete item to notifications

--- src/components/NotificationList.jsx
+++ src/components/NotificationList.jsx
@@ -8,32 +8,44 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Divider from "@mui/material/Divider";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import _isEmpty from "lodash/isEmpty";
+import IconButton from "@mui/material/IconButton";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 const NotificationList = () => {
-	const { items } = useContext(NotificationContext);
+	const { items, remove } = useContext(NotificationContext);
+
+	const onDelete = item => {
+		remove(item);
+	};
 
 	return (
 		<div>
-			<List
-				sx={{
-					width: "100%",
-					maxWidth: 360,
-					bgcolor: "background.paper",
-					mt: 2,
-				}}
-			>
-				{_map(items, (item, key) => (
-					<ListItem key={key}>
-						<ListItemAvatar>
-							<Avatar>
-								<NotificationsNoneIcon />
-							</Avatar>
-						</ListItemAvatar>
-						<ListItemText primary={item} />
-						<Divider variant="middle" />
-					</ListItem>
-				))}
-			</List>
+			{!_isEmpty(items) && (
+				<List
+					sx={{
+						width: "100%",
+						maxWidth: 360,
+						bgcolor: "background.paper",
+						mt: 2,
+					}}
+				>
+					{_map(items, (item, key) => (
+						<ListItem key={key}>
+							<ListItemAvatar>
+								<Avatar>
+									<NotificationsNoneIcon />
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText primary={item} />
+							<Divider variant="middle" />
+							<IconButton onClick={() => onDelete(item)}>
+								<DeleteOutlineIcon />
+							</IconButton>
+						</ListItem>
+					))}
+				</List>
+			)}
 		</div>
 	);
 };
