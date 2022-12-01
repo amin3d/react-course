b3b33501300e157a173a21fffc6a1a9a54c8d651
Hossein Mosalli <mosali@payrad.org>
2022-12-01 08:19:49 +0330

    feat: add login and dashboard pages

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,105 @@
+import { useContext, useEffect, useRef } from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Template } from "../../layout";
+import _map from "lodash/map";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import { useTodos } from "../../hooks";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { NotificationContext } from "../../context";
+import { NotificationList } from "../../components";
+
+const Dashboard = () => {
+	const [todos, getTodos] = useTodos();
+
+	useEffect(() => {
+		getTodos();
+	}, [getTodos]);
+
+	const { add } = useContext(NotificationContext);
+	const notifTextRef = useRef();
+
+	const onAddNotification = () => {
+		add(notifTextRef.current.value);
+	};
+
+	return (
+		<Box width="100%" height="100%" bgcolor="#f5f5f5">
+			<Box
+				height="100%"
+				display="flex"
+				alignItems="center"
+				justifyContent="flex-start"
+			>
+				<Template>
+					<Container maxWidth="xl">
+						<Grid container spacing={2} mt={2}>
+							<Grid item xs={6}>
+								<Grid container spacing={2}>
+									<Grid item xs={12}>
+										<TextField
+											inputRef={notifTextRef}
+											label="متن اعلان"
+											variant="outlined"
+											size="small"
+											sx={{ mr: 2 }}
+										/>
+										<Button
+											color="success"
+											variant="contained"
+											onClick={onAddNotification}
+										>
+											افزودن اعلان
+										</Button>
+									</Grid>
+									<Grid item xs={12}>
+										<NotificationList />
+									</Grid>
+								</Grid>
+							</Grid>
+							<Grid item xs={6}>
+								{_map(todos, ({ id, title, completed }) => (
+									<Card sx={{ mb: 1 }}>
+										<CardContent>
+											<Box
+												display="flex"
+												justifyContent="flex-start"
+												alignItems="center"
+											>
+												<Box display="flex" mr={1}>
+													{completed ? (
+														<CheckIcon color="success" />
+													) : (
+														<CloseIcon color="error" />
+													)}
+												</Box>
+												<Typography
+													sx={{
+														fontSize: 16,
+														marginBottom: 0,
+													}}
+													color="slategray"
+													gutterBottom
+												>
+													{title}
+												</Typography>
+											</Box>
+										</CardContent>
+									</Card>
+								))}
+							</Grid>
+						</Grid>
+					</Container>
+				</Template>
+			</Box>
+		</Box>
+	);
+};
+
+export default Dashboard;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,95 @@
+import { useContext, useRef, useState } from "react";
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { AuthContext } from "../../context";
+import _isEmpty from "lodash/isEmpty";
 
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const { onLogin } = useContext(AuthContext);
+	const usernameRef = useRef();
+	const passwordRef = useRef();
+
+	const loginUser = () => {
+		if (_isEmpty(username)) {
+			usernameRef.current.focus();
+		} else if (_isEmpty(password)) {
+			passwordRef.current.focus();
+		} else {
+			console.log("here...");
+			onLogin(username, password);
+		}
+	};
+
+	const onChangeUsername = e => {
+		setUsername(e.target.value);
+	};
+
+	const onChangePassword = e => {
+		setPassword(e.target.value);
+	};
+
 	return (
-		<Box mt={20}>
-			<Grid container spacing={3}>
-				<Grid item xs={12}>
-					<Box display="flex" justifyContent="center">
-						<TextField label="نام کاربری" variant="outlined" />
-					</Box>
-				</Grid>
-				<Grid item xs={12}>
-					<Box display="flex" justifyContent="center">
-						<TextField label="کلمه عبور" variant="outlined" />
-					</Box>
-				</Grid>
-				<Grid item xs={12}>
-					<Box display="flex" justifyContent="center">
-						<Button variant="contained">ورود</Button>
-					</Box>
-				</Grid>
-			</Grid>
+		<Box width="100vw" height="100vh" bgcolor="#f5f5f5">
+			<Box
+				height="100%"
+				display="flex"
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Container maxWidth="sm">
+					<Card sx={{ minWidth: 275 }}>
+						<CardContent>
+							<Grid container spacing={3} pt={2}>
+								<Grid item xs={2} />
+								<Grid item xs={8}>
+									<TextField
+										inputRef={usernameRef}
+										fullWidth
+										label="نام کاربری"
+										variant="outlined"
+										size="small"
+										value={username}
+										onChange={onChangeUsername}
+									/>
+								</Grid>
+								<Grid item xs={2} />
+								<Grid item xs={2} />
+								<Grid item xs={8}>
+									<TextField
+										inputRef={passwordRef}
+										fullWidth
+										label="کلمه عبور"
+										variant="outlined"
+										size="small"
+										type="password"
+										value={password}
+										onChange={onChangePassword}
+									/>
+								</Grid>
+								<Grid item xs={2} />
+								<Grid item xs={2} />
+								<Grid item xs={8}>
+									<Button
+										fullWidth
+										variant="contained"
+										onClick={loginUser}
+									>
+										ورود
+									</Button>
+								</Grid>
+								<Grid item xs={2} />
+							</Grid>
+						</CardContent>
+					</Card>
+				</Container>
+			</Box>
 		</Box>
 	);
 };
