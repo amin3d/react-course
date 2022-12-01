28dced77dfc063f97db46d9be47efcfccc08b331
Hossein Mosalli <mosali@payrad.org>
2022-12-01 08:21:25 +0330

    feat: font settings

--- src/common/mui-theme.js
+++ src/common/mui-theme.js
@@ -1,8 +1,10 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-    direction: "rtl",
+	direction: "rtl",
 	typography: {
+		fontFamily: "iranyekan",
+		fontSize: 13,
 		button: {
 			textTransform: "none",
 		},
