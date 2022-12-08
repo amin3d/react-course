f88eab1d4d1ad4990de931a2684c3fec556eabe6
Hossein Mosalli <mosali@payrad.org>
2022-12-08 09:22:49 +0330

    feat: adds BrowserRouter provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<AuthContextProvider>
-		<App />
-	</AuthContextProvider>
+	<BrowserRouter>
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
