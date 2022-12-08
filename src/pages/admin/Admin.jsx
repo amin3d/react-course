a3e02f9cf5f26fe57a6821462653c5aec54eb6d1
Hossein Mosalli <mosali@payrad.org>
2022-12-08 11:10:08 +0330

    wip: temp solution

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,20 +1,23 @@
-import { useContext, useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context";
+// import { useContext, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+// import { AuthContext } from "../../context";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { Template } from "../../layout";
 
 const Admin = () => {
-	const { isLoggedIn } = useContext(AuthContext);
+	// const { isLoggedIn } = useContext(AuthContext);
 
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	useEffect(() => {
-		if (!isLoggedIn) {
-			navigate("/login");
-		}
-	}, [isLoggedIn, navigate]);
+	//FIXME: fix async problem to read isLoggedIn from local storage
+	// useEffect(() => {
+	// 	console.log(isLoggedIn);
+	// 	if (!isLoggedIn) {
+	// 		console.log("here...");
+	// 		// navigate("/login");
+	// 	}
+	// }, [isLoggedIn, navigate]);
 
 	return (
 		<Box width="100%" height="100%" bgcolor="#f5f5f5">
