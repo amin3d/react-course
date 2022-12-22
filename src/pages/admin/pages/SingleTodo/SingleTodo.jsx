b17e23f5c99194faefcc92168a696a3b0c282f2e
Hossein Mosalli <mosali@payrad.org>
2022-12-22 10:06:12 +0330

    feat: adds back button and get todo info

--- src/pages/admin/pages/SingleTodo/SingleTodo.jsx
+++ src/pages/admin/pages/SingleTodo/SingleTodo.jsx
@@ -1,14 +1,31 @@
+import { Fragment, useEffect } from "react";
 import Box from "@mui/material/Box";
-import { Fragment } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useGetTodo } from "../../../../hooks";
+import Button from "@mui/material/Button";
 
 const SingleTodo = () => {
 	const { id } = useParams();
+	const [info, getTodo] = useGetTodo();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (id) {
+			getTodo(id);
+		}
+	}, [getTodo, id]);
 
 	return (
 		<Fragment>
-			<Box>Todo ID: {id}</Box>
-			<Outlet />
+			<Box>ID: {id}</Box>
+			<Box>Name: {info.todo}</Box>
+			<Button
+				variant="outlined"
+				sx={{ mt: 2 }}
+				onClick={() => navigate(-1)}
+			>
+				بازگشت
+			</Button>
 		</Fragment>
 	);
 };
